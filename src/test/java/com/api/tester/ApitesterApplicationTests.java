78a306ef92eef309135d2b298cb87175b7c6e1e4
nishithsai00 <bootgaduwastegadu@gmail.com>
2026-05-16 19:37:22 +0530

    project init

---
+++ src/test/java/com/api/tester/ApitesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.tester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
